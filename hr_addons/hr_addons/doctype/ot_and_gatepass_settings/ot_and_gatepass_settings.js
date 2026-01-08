b595f00aba504632b4b60b446236bb6eeb3681a3
Kaajal-Chhattani <kaajal.chhattani@aurigait.com>
2026-01-08 17:00:08 +0530

    Added settings

---
+++ hr_addons/hr_addons/doctype/ot_and_gatepass_settings/ot_and_gatepass_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Kaajal and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("OT and Gatepass Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
